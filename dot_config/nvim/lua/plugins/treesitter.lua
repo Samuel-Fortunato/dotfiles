0c6c1aca223650f1c29c6349deb6fb36e62ba8fd
Samuel-Fortunato <samff2006@gmail.com>
2026-06-16 19:11:11 +0100

    autopairs tweaking

---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local treesitter = require("nvim-treesitter")
+		treesitter.install({
+			"c",
+			"latex"
+		})
+	end
+}
