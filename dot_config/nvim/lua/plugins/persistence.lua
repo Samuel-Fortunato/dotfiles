75156345c1944a8e8d64629186ddb4a7e63f34fd
Samuel-Fortunato <samff2006@gmail.com>
2026-07-03 22:08:26 +0100

    nvim - add session manager persistence.nvim

---
+++ dot_config/nvim/lua/plugins/persistence.lua
@@ -0,0 +1,11 @@
+return {
+	"folke/persistence.nvim",
+	event = "BufReadPre",
+	keys = {
+		{ "<leader>sd", function() require("persistence").load() end,                desc = "Load the session for the current directory" },
+		{ "<leader>sl", function() require("persistence").select() end,              desc = "Select a session to load" },
+		{ "<leader>ss", function() require("persistence").load({ last = true }) end, desc = "Load the last session" },
+		{ "<leader>sS", function() require("persistence").stop() end,                desc = "Stop Persistence => session won't be saved on exit" },
+	},
+	opts = {}
+}
