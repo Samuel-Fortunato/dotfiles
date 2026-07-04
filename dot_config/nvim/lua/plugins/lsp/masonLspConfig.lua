2c601866458de81bdeefc0d9f10594b6cc88d2e7
Samuel-Fortunato <samff2006@gmail.com>
2026-07-04 17:54:32 +0100

    nvim - remove crap mason-lspconfig code added by stupid LLM

--- dot_config/nvim/lua/plugins/lsp/masonLspConfig.lua
+++ dot_config/nvim/lua/plugins/lsp/masonLspConfig.lua
@@ -9,14 +9,14 @@ return {
 			"clangd",
 			"texlab"
 		},
-		handlers = {
-			function(server_name)
-				local capabilities = require("blink.cmp").get_lsp_capabilities()
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-		},
+		-- handlers = {
+		-- 	function(server_name)
+		-- 		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		-- 		require("lspconfig")[server_name].setup({
+		-- 			capabilities = capabilities,
+		-- 		})
+		-- 	end,
+		-- },
 	},
 	dependencies = {
 		"mason-org/mason.nvim",
