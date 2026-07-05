d1b783687224f45b41eb6f983caf93b80021728f
Samuel-Fortunato <samff2006@gmail.com>
2026-07-05 16:52:11 +0100

    nvim - obsidian plugin

---
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,10 @@
+return {
+	"obsidian-nvim/obsidian.nvim",
+	event = "VeryLazy",
+	opts = {
+		legacy_commands = false,
+		workspaces = {
+			{ name = "CMTM", path = "~/Documents/CMTM Vault/" }
+		},
+	},
+}
