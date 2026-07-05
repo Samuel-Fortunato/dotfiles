536b8bfc37768aba5f08a431efdc4d01a985d995
Samuel-Fortunato <samff2006@gmail.com>
2026-07-05 17:03:56 +0100

    remove unecessary blink.cmp configs

--- dot_config/nvim/lua/plugins/blinkCmp.lua
+++ dot_config/nvim/lua/plugins/blinkCmp.lua
@@ -25,25 +25,13 @@ return {
 			providers = {
 				snippets = {
 					min_keyword_length = 2,
-					-- score_offset = 5,
 				},
 				vimtex = {
 					name = "vimtex",
-					-- min_keyword_length = 2,
 					module = "blink.compat.source",
-					-- score_offset = 4,
-				},
-				lsp = {
-					-- min_keyword_length = 3,
-					-- score_offset = 3,
-				},
-				path = {
-					-- min_keyword_length = 3,
-					-- score_offset = 2,
 				},
 				buffer = {
 					min_keyword_length = 5,
-					-- score_offset = 1,
 				},
 			},
 		},
