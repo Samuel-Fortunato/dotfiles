923b8350c6697ce9fe059f1433ac975530a7cca6
Samuel-Fortunato <samff2006@gmail.com>
2026-07-06 23:25:31 +0100

    nvim - add bufferline

---
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,18 @@
+return {
+	'akinsho/bufferline.nvim',
+	-- enabled = false,
+	lazy = false,
+	keys = {
+		{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+
+		{ "<leader>bc", "<cmd>bdelete<cr>", desc = "Close Buffer" },
+
+		{ "<C-h>", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer left" },
+		{ "<C-l>", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer right" },
+
+		{ "<leader>bb", "<cmd>BufferLinePick<cr>", desc = "Pick Buffer" },
+	},
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	opts = { }
+}
